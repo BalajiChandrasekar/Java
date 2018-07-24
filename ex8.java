400b4c0bf648b4ac4bd7c029f2db2b3b1008109f
Balaji C <40636651+BalajiChandrasekar@users.noreply.github.com>
2018-07-24 18:23:02 +0530

    Create ex8.java

---
+++ ex8.java
@@ -0,0 +1,13 @@
+package ex8;
+
+public class ex8 {
+	public static void main(String args[])
+	{
+		System.out.println("   J     A   V      V      A ");
+        System.out.println("   J    A  A  V    V     A  A");
+        System.out.println("   J   AAAAAA  V  V     AAAAAA");
+        System.out.println("J  J  A      A   V     A     A");
+        System.out.println("JJJJ A        A       A       A");
+	}
+
+}
